2567028ece63c17ad7aa151008d7249a88de80a6
Scott Aaron <dr_saaron@yahoo.com>
2022-08-01 10:48:06 -0500

    ,

--- src/main/java/com/blazartech/products/qotdp/data/access/impl/spring/jpa/QuoteOfTheDayDALSpringJpaImpl.java
+++ src/main/java/com/blazartech/products/qotdp/data/access/impl/spring/jpa/QuoteOfTheDayDALSpringJpaImpl.java
@@ -18,7 +18,6 @@ import com.blazartech.products.qotdp.data.access.impl.spring.jpa.repos.QuoteData
 import com.blazartech.products.qotdp.data.access.impl.spring.jpa.repos.QuoteOfTheDayDataRepository;
 import com.blazartech.products.qotdp.data.access.impl.spring.jpa.repos.SrcValDataRepository;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.List;
